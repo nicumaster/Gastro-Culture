23fc17f346dd48ff786a413a363263848c6df876
Lachenal Nicolas <blachn@bbc-be.local>
2018-04-18 08:27:56 +0200

    navbar update

--- index.php
+++ index.php
@@ -19,16 +19,13 @@
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Länder</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Rezepte</a>
+                <a class="nav-link" href="#">Home</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Über uns</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
             </li>
         </ul>
     </div>
