d17bbdbf02665892845cc4ce3cf362d0a0da966d
simu21 <simonecrupi21@gmail.com>
2018-04-19 10:18:32 +0200

    desing

--- view/default_index.php
+++ view/default_index.php
@@ -5,7 +5,7 @@
         <div class="home_map_russia_div"><a href="Land?region=ru"></a></div>
         <div class="home_map_usa_div"><a href="Land?region=us"></a></div>
         <div class="home_map_south_america_div"><a href="Land?region=sa"></a></div>
-        <div class="home_map_africa_div"><a href="Land"></a></div>
-        <div class="home_map_asia_div"><a href="Land"></a></div>
-        <div class="home_map_australia_div"><a href="Land"></a></div>
+        <div class="home_map_africa_div"><a href="LandLand?region=afr"></a></div>
+        <div class="home_map_asia_div"><a href="Land?region=as"></a></div>
+        <div class="home_map_australia_div"><a href="Land?region=aus"></a></div>
 </div>
